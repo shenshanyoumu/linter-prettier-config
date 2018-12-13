315ba9583f5b512de925bc89868b0b48f5d67d1f
duduxiong <xianglinzong_cd@keruyun.com>
2018-12-13 12:52:50 +0800

    initial add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # linter-prettier-config
-前端开发中关于Linter工具和Prettier工具的一些配置积累
+
+前端开发中关于 Linter 工具和 Prettier 工具的一些配置积累，包括 styleLint、ESLint、postCSS 等
---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    "class-methods-use-this": 0,
+    "comma-dangle": 0,
+    "consistent-return": 0,
+    "no-underscore-dangle": 0,
+    "no-trailing-spaces": 0,
+    "global-require": 0,
+    "import/extensions": 0,
+    "import/no-dynamic-require": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/no-named-as-default": 0,
+    "import/no-unresolved": 0,
+    "import/prefer-default-export": 0,
+    "react/forbid-prop-types": 0,
+    "react/jsx-no-bind": 0,
+    "react/jsx-wrap-multilines": 0
+  }
+};
